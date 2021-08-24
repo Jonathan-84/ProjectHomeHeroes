d4eaf4a3bbeec3ef1e2dac40a68e63e2b97d0ac7
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-08-23 23:49:51 -0400

    making data changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,19 +4,37 @@ const { Kids, Tasks, Users } = require('../models');
 
 // on section one of Chapter 14, playing with the getting of data --- 
 //only touched this page and the homepage. view
+
+
 router.get('/', (req, res) => {
-  res.render('homepage', {
-    id: 1,
-    post_url: 'http://localhost:3001/api/kids',
-    title: 'Kids',
-    child_name: "",
-    current_points: "",
-    banked_points: "",
-    tasks: [{}, {}],
-    user: {
-      name: ""
-    }
-  });
+  Kids.findAll({
+    attributes: [
+      'id',
+      'child_name',
+      'current_points',
+      'banked_points',
+      'users_id',
+    //  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Tasks,
+        attributes: ['id', 'task_name', 'task_points', 'kids_id'],
+      },
+      {
+        model: Users,
+        attributes: ['name']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      res.render('homepage', dbPostData[0]);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
