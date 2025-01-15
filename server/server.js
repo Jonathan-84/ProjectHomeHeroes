69993a8973ef1153229e508a401f78aec201152c
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-14 22:53:08 -0500

    hoping this fixes the issue for using react instead of view

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve static files from the React app's build folder
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // Handle any requests that don’t match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
 });
 
 // Turn on routes mostly for API endpoints
