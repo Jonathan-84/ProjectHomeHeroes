1d39d17e9a30dd34619c7957f3815f3b23adbe5b
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-14 22:46:34 -0500

    hoping this fixes the issue for using react instead of view

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 var express = require('express');
 const { authMiddleware } = require("./utils/auth");
-// const session = require('express-session');
 var app = express();
 var path = require('path');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,29 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
+// Serve static files from the React app's build folder
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle any requests that don’t match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+// Turn on routes mostly for API endpoints
 app.use(routes);
 
-// /// add express sessions language
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Apply authMiddleware
+app.use(authMiddleware);
 
-// const sess = {
-//   secret: 'UData',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-app.use( authMiddleware);
-
-
-// turn on connection to db and server
-sequelize.sync({ alter: true  }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// Turn on connection to DB and server
+sequelize.sync({ alter: true }).then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
