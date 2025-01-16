6a8b9ccb1d8a8d0881e998bde57f90bbe3a2ad92
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-16 00:19:54 -0500

    reorder serverjs seemed to work

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//builds but only login works
+
 var express = require('express');
 const { authMiddleware } = require("./utils/auth");
 var app = express();
@@ -14,18 +16,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve static files from the React app's build folder
 app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
-// Handle any requests that don’t match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
-});
-
 // Turn on routes mostly for API endpoints
 app.use(routes);
 
 // Apply authMiddleware
 app.use(authMiddleware);
 
+// Handle any requests that don’t match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
+});
+
 // Turn on connection to DB and server
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
+
+// works, including locally
+// var express = require('express');
+// const { authMiddleware } = require("./utils/auth");
+
+// var app = express();
+// var path = require('path');
+// const sequelize = require('./config/connection');
+// const routes = require('./controllers');
+
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // turn on routes
+// app.use(routes);
+
+// app.use( authMiddleware);
+
+// // turn on connection to db and server
+// sequelize.sync({ alter: true  }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
