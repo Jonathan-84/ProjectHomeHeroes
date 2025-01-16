56eac54f067d2531c7cf2f8fbb80fa064affde0e
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-16 01:04:46 -0500

    only homepage worked

--- server/server.js
+++ server/server.js
@@ -12,24 +12,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// seems like this is biggest diffeence
+// // Serve static files from the React app's build folder
+// app.use(express.static(path.join(__dirname, '..', 'client/build')));
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 // Turn on routes mostly for API endpoints
 app.use(routes);
 
-// Apply authMiddleware
+// // Apply authMiddleware
 app.use(authMiddleware);
 
+// // Handle any requests that don’t match the ones above
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
+// });
 
-// Serve static files from the React app's build folder
-app.use(express.static(path.join(__dirname, '..', 'client/build')));
-
-
-
-// Handle any requests that don’t match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
-});
-
-// Turn on connection to DB and server
+// // Turn on connection to DB and server
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
