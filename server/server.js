78564a07896ef0ae78933d95bffbc5201d9f0e27
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-18 09:25:44 -0500

    attempted run build to see if this workes better

--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 // Turn on routes mostly for API endpoints
 app.use(routes);
 
