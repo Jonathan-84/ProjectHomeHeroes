76dc9a2356490d73f03b28407707a80dba709165
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-18 09:48:59 -0500

    attempted run build to see if this workes better

--- server/server.js
+++ server/server.js
@@ -1,48 +1,29 @@
 //builds but only login works
 
-const express = require('express');
-const path = require('path');
+var express = require('express');
 const { authMiddleware } = require("./utils/auth");
+var app = express();
+var path = require('path');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Apply authMiddleware for API routes
-app.use('./api', authMiddleware);
+// Turn on routes mostly for API endpoints
+app.use(routes);
 
-// Turn on API routes
-app.use('./api', routes);
+// // Apply authMiddleware
+app.use(authMiddleware);
 
-// Serve static files from the React app's build folder
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // Catch-all route to serve React's index.html
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
-  });
-}
-
-// Catch-all route for development (non-API requests)
-app.get('*', (req, res, next) => {
-  if (process.env.NODE_ENV !== "production") {
-    res.send('API is running...');
-  } else {
-    next();
-  }
-});
-
-// Turn on connection to DB and server
+// // Turn on connection to DB and server
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
-
-
 
 // works, including locally
 // var express = require('express');
