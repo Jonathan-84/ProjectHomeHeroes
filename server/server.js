b82a7dcc399f9c02b30cef1aaa0248e78371750a
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-18 12:44:45 -0500

    incremental to start adding react back in heroku, hoping api and everything performs correctly again

--- server/server.js
+++ server/server.js
@@ -12,25 +12,24 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build'))); // Serving static files from React build
 
-// // Apply authMiddleware
+// Apply authMiddleware
 app.use(authMiddleware);
+
 // Turn on routes mostly for API endpoints
 app.use(routes);
 
 // Serve React frontend 
-// app.use((req, res) => 
-//   { res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
-//   });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// // Turn on connection to DB and server
+// Turn on connection to DB and server
 sequelize.sync({ alter: false }).then(() => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
+
 
 // works, including locally
 // var express = require('express');
