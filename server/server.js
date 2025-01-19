d60574d7e6c91a4e2128dcc6ff592ceee0cf73af
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 09:46:56 -0500

    added the .js to controllers where missing

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index"));
 });
 
 // Turn on routes mostly for API endpoints
