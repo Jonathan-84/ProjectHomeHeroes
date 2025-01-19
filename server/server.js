b60b68f3f8e413bfc8265b5132467be2ab6fd60e
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 10:06:44 -0500

    added the .js to controllers where missing

--- server/server.js
+++ server/server.js
@@ -1,56 +1,8 @@
-//this doesn't server up react componently correctly
-//but the backend behaves correctly
+// //this doesn't server up react componently correctly
+// //but the backend behaves correctly
 
-var express = require('express');
-// const { authMiddleware } = require("./utils/auth");
-var app = express();
-var path = require('path');
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
-
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-
-
-// // Apply authMiddleware
-// app.use(authMiddleware);
-
-
-
-// app.get('', (req, res) => {
-//   res.json({ message: 'Hello from the server!' });
-// });
-
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-// Turn on routes mostly for API endpoints
-app.use(routes);
-
-app.get("/health", (req, res) => {
-  res.status(200).send("Server is healthy!");
-});
-
-
-// // Turn on connection to DB and server
-sequelize.sync({ alter: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-});
-
-// works, including locally
 // var express = require('express');
-// const { authMiddleware } = require("./utils/auth");
-
+// // const { authMiddleware } = require("./utils/auth");
 // var app = express();
 // var path = require('path');
 // const sequelize = require('./config/connection');
@@ -60,14 +12,62 @@ sequelize.sync({ alter: false }).then(() => {
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-// // turn on routes
+
+
+// // // Apply authMiddleware
+// // app.use(authMiddleware);
+
+
+
+// // app.get('', (req, res) => {
+// //   res.json({ message: 'Hello from the server!' });
+// // });
+
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// // Turn on routes mostly for API endpoints
 // app.use(routes);
 
-// app.use( authMiddleware);
-
-// // turn on connection to db and server
-// sequelize.sync({ alter: true  }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
+// app.get("/health", (req, res) => {
+//   res.status(200).send("Server is healthy!");
 // });
+
+
+// // // Turn on connection to DB and server
+// sequelize.sync({ alter: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// });
+
+// works, including locally
+var express = require('express');
+const { authMiddleware } = require("./utils/auth");
+
+var app = express();
+var path = require('path');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+app.use( authMiddleware);
+
+// turn on connection to db and server
+sequelize.sync({ alter: true  }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
