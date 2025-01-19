d12675d3ea7071da7c318bb2cb049b5b786197dd
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 09:40:21 -0500

    added the .js to controllers where missing

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,7 +4,7 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: function({ req }) {
+  authMiddleware: (req, res, next) => {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -16,22 +16,27 @@ module.exports = {
         .trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
-      return req;
+      return next();
     }
 
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
+      // optionally, you can respond with an error status
+      return res.status(401).json({ message: 'Invalid token' });
     }
 
-    return req;
+    // pass control to the next middleware
+    next();
   },
-  signToken: function({ name, email, id, role, reset_hint, reset_code }) {
+  
+  signToken: ({ name, email, id, role, reset_hint, reset_code }) => {
     const payload = { name, email, id, role, reset_hint, reset_code };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   }
 };
