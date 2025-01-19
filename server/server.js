68da46219615a9e9b2cd1adbb5cde52d816f99b9
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 10:22:22 -0500

    added the .js to controllers where missing

--- server/server.js
+++ server/server.js
@@ -70,9 +70,10 @@ app.use(routes);
 app.use( authMiddleware);
 
 // // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
