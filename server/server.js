ee3373ac8e3a3caf495057b26934968fff270e4f
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 13:34:22 -0500

    still trying to fix the api/front end to wrok as planned when deployed

--- server/server.js
+++ server/server.js
@@ -47,31 +47,72 @@
 //   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 // });
 
-// works, including locally
+// works, including locally ---
+// var express = require('express');
+// const { authMiddleware } = require("./utils/auth");
+
+// var app = express();
+// var path = require('path');
+// const sequelize = require('./config/connection');
+// const routes = require('./controllers');
+
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../client/public')));
+
+// // turn on routes
+// app.use(routes);
+
+// app.use( authMiddleware);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// // turn on connection to db and server
+// sequelize.sync({ alter: false  }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+// tests below
 var express = require('express');
 const { authMiddleware } = require("./utils/auth");
-
-var app = express();
 var path = require('path');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
 const PORT = process.env.PORT || 3001;
+const isProd = process.env.NODE_ENV === 'production';
+
+var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/public')));
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // turn on routes
 app.use(routes);
 
-app.use( authMiddleware);
+app.use(authMiddleware);
 
-app.get("*", (req, res) => {
+// API routes
+app.get('/api/*', (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get("*", (req, res) => {
+  if (isProd || req.headers['x-local-dev']) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  } else {
+    res.status(404).send('Not found');
+  }
+});
+
 // turn on connection to db and server
-sequelize.sync({ alter: false  }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ alter: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
