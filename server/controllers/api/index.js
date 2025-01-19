0a5cd4155350c980b02e4763b968450814c07520
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-19 09:13:22 -0500

    added the .js to controllers where missing

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 const usersRoutes = require('./users-routes.js');
 const tasksRoutes = require('./tasks-routes.js');
-const kidsRoutes= require('./kids-routes');
-const rewardsRoutes= require('./rewards-routes');
-const redemptionsRoutes= require('./redemptions-routes');
+const kidsRoutes= require('./kids-routes.js');
+const rewardsRoutes= require('./rewards-routes.js');
+const redemptionsRoutes= require('./redemptions-routes.js');
 
 router.use('/users', usersRoutes);
 router.use('/tasks', tasksRoutes);
