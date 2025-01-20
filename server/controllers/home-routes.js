e7c69afd85fd195333a486fabdc6cedac8f4c00a
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-20 14:30:09 -0500

    rvert back to old routes

--- server/controllers/home-routes.js
+++ server/controllers/home-routes.js
@@ -1,6 +1,46 @@
+// const router = require('express').Router();
+// const path = require('path');
+// const { Kids, Tasks, Users } = require('../models');
+
+// // Define the /login route
+// router.get('/login', (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// // Define other routes
+// router.get('/', (req, res) => {
+//   Kids.findAll({
+//     attributes: [
+//       'id',
+//       'child_name',
+//       'avatar',
+//       'current_points',
+//       'users_id',
+//     ],
+//     include: [
+//       {
+//         model: Tasks,
+//         attributes: ['id', 'task_name', 'task_points', 'kids_id'],
+//       },
+//       {
+//         model: Users,
+//         attributes: ['name', 'id']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       res.json(dbPostData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// module.exports = router;
+
 const router = require('express').Router();
 const path = require('path');
-const { Kids, Tasks, Users } = require('../models');
 
 // Define the /login route
 router.get('/login', (req, res) => {
@@ -9,32 +49,8 @@ router.get('/login', (req, res) => {
 
 // Define other routes
 router.get('/', (req, res) => {
-  Kids.findAll({
-    attributes: [
-      'id',
-      'child_name',
-      'avatar',
-      'current_points',
-      'users_id',
-    ],
-    include: [
-      {
-        model: Tasks,
-        attributes: ['id', 'task_name', 'task_points', 'kids_id'],
-      },
-      {
-        model: Users,
-        attributes: ['name', 'id']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      res.json(dbPostData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.json({ message: "Welcome to the API!" });
 });
 
 module.exports = router;
+
