4b60be4131565c1dc6e1cfbeec2ea0d2ba803d3f
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-20 13:55:20 -0500

    rvert back to old routes

--- server/controllers/home-routes.js
+++ server/controllers/home-routes.js
@@ -1,50 +1,50 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Kids, Tasks, Users } = require('../models');
+// const router = require('express').Router();
+// const sequelize = require('../config/connection');
+// const { Kids, Tasks, Users } = require('../models');
 
-// on section one of Chapter 14, playing with the getting of data --- 
-//only touched this page and the homepage. view
+// // on section one of Chapter 14, playing with the getting of data --- 
+// //only touched this page and the homepage. view
 
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
 
-  Kids.findAll({
-    attributes: [
-      'id',
-      'child_name',
-      'avatar',
-      'current_points',
-      'users_id',
-    //  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Tasks,
-        attributes: ['id', 'task_name', 'task_points', 'kids_id'],
-      },
-      {
-        model: Users,
-        attributes: ['name', 'id']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      // pass a single post object into the homepage template
-      res.json(dbPostData[0]);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-
-  // res.render('login');
+//   Kids.findAll({
+//     attributes: [
+//       'id',
+//       'child_name',
+//       'avatar',
+//       'current_points',
+//       'users_id',
+//     //  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+//     ],
+//     include: [
+//       {
+//         model: Tasks,
+//         attributes: ['id', 'task_name', 'task_points', 'kids_id'],
+//       },
+//       {
+//         model: Users,
+//         attributes: ['name', 'id']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       // pass a single post object into the homepage template
+//       res.json(dbPostData[0]);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
 // });
 
-module.exports = router;
+// // router.get('/login', (req, res) => {
+// //   if (req.session.loggedIn) {
+// //     res.redirect('/');
+// //     return;
+// //   }
+
+//   // res.render('login');
+// // });
+
+// module.exports = router;
