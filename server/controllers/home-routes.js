5184f263c5a4699beae4b0cf66f23b1c404c096e
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-20 14:21:26 -0500

    rvert back to old routes

--- server/controllers/home-routes.js
+++ server/controllers/home-routes.js
@@ -1,11 +1,40 @@
-// 
-
 const router = require('express').Router();
-const { Users } = require('../models');
+const path = require('path');
+const { Kids, Tasks, Users } = require('../models');
 
 // Define the /login route
 router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// Define other routes
+router.get('/', (req, res) => {
+  Kids.findAll({
+    attributes: [
+      'id',
+      'child_name',
+      'avatar',
+      'current_points',
+      'users_id',
+    ],
+    include: [
+      {
+        model: Tasks,
+        attributes: ['id', 'task_name', 'task_points', 'kids_id'],
+      },
+      {
+        model: Users,
+        attributes: ['name', 'id']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,15 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-// const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
-
-// Define the /login route in index.js if it's not included elsewhere 
-router.get('/login', (req, res) => 
-  { res.sendFile(path.join(__dirname, "../client/build/index.html")); });
 
 router.use((req, res) => {
   res.status(404).end();
