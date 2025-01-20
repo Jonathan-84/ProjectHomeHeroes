0503bce7eb4a7caa5c0e2ffefe740e31f794e420
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-20 12:35:44 -0500

    rvert back to old routes

--- server/controllers/home-routes.js
+++ server/controllers/home-routes.js
@@ -38,13 +38,13 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
+// router.get('/login', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
 
-  res.render('login');
-});
+  // res.render('login');
+// });
 
 module.exports = router;
