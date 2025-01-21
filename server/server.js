9dcb97475be3fc9f218613da98ce32f259c1f267
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-21 00:03:05 -0500

    trying to remove confusing routes

--- server/server.js
+++ server/server.js
@@ -123,7 +123,7 @@ app.use(routes);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 // });
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
