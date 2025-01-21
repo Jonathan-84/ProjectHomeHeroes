8767528772f937111b976c7bb9d53a97307cb301
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-21 00:24:00 -0500

    trying to remove confusing routes

--- server/server.js
+++ server/server.js
@@ -110,6 +110,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // // Serve static files from the React app
 // app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/*', (req, res) => { res.sendFile(path.join(__dirname, '../client/build', 'index.html')); 
+
+});
+
 
 // Turn on auth middleware and routes
 app.use(authMiddleware);
