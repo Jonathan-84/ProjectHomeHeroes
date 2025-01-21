12421b9c1ff242d9b7a40a3885c5acb208901f55
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-21 15:55:07 -0500

    checking api url

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -11,9 +11,9 @@ const root = document.getElementById("root");
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.createRoot(root).render(
   // <React.StrictMode>
-     <BrowserRouter>
+     <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   // </React.StrictMode>
 );
 
