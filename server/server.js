00e08429e306cce4cb3dea00d5dbeb751aac28e5
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-23 15:03:39 -0500

    still issue deploying heroku and front/backend

--- server/server.js
+++ server/server.js
@@ -57,11 +57,13 @@ app.use(express.urlencoded({ extended: true }));
 // Turn on auth middleware (apply after defining API routes)
 app.use(authMiddleware);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 // Use API routes with a prefix
 app.use('/api', apiRoutes);  // Make sure to use the /api prefix
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Catch-all handler to serve React app for non-API routes
 app.get('*', (req, res) => {
