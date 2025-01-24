f566a5ce349cd2c3b283a898aaa6ba831ba6b2c8
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-01-24 01:08:48 -0500

    adding penalties and layout improve

--- server/models/PointHistory.js
+++ server/models/PointHistory.js
@@ -41,7 +41,7 @@ PointHistory.init(
     {
     
         sequelize,
-        modelName: 'pointhistory',
+        modelName: 'redemptions',
         freezeTableName: true,
         timestamps: false,
         underscored: true,
